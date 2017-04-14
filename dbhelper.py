dc658954c4181c564c4c8755cc7350ce46aace61
Сергей Малашин <s1310601@iivt.donstu.ru>
2017-04-15 00:49:43 +0300

    little fix in dbhelper

--- dbhelper.py
+++ dbhelper.py
@@ -10,7 +10,7 @@ def create_worker(key, args):
             date_of_hire=datetime.now(),
             status=Workers_status[0],
             phone=args['phone'],
-            api_key=Keys(key='0', role=Roles.get(name='Worker')),
+            api_key='test',
             car_number=args['car_number'],
             company=Companies.get(lambda c: c.api_key.key == key),
             supported_car_type=Car_type[args['car_type']])
